84cdf50691dde21cb7deaffb49c3bab8ffe98e70
Quinn <quantumjump451@gmail.com>
2023-12-10 01:13:30 +0000

    Day 3 Part 2

--- src/day03/bin/main.rs
+++ src/day03/bin/main.rs
@@ -29,12 +29,80 @@ struct Number {
     bounding_box: BoundingBox,
 }
 
+#[derive(Clone, Debug)]
+struct Symbol {
+    ch: char,
+    coord: Coordinate,
+}
+
 #[derive(Clone)]
 struct StateMachine {
     coord: Coordinate,
     current_number: Option<Number>,
 }
 
+impl StateMachine {
+    fn consume_number(&mut self, ch: char) -> (Option<Number>, Option<Symbol>) {
+        match (ch, self.current_number.clone()) {
+            ('.', Some(num)) => {
+                // End of a number; update.
+                self.current_number = None;
+                (Some(num), None)
+            },
+            ('.', None) => (None, None), // Not in a number
+            (c, None) if c.is_ascii_digit() => {
+                // Start of a number
+                let number = Number{
+                    value: c.to_digit(10).unwrap() as usize,
+                    bounding_box: BoundingBox{
+                        start: Coordinate{
+                            x: self.coord.x-1,
+                            y: self.coord.y-1,
+                        },
+                        end: Coordinate {
+                            x: self.coord.x+1,
+                            y: self.coord.y+1,
+                        }
+                    }
+                };
+                self.current_number = Some(number);
+                (None, None)
+            },
+            (c, Some(num)) if c.is_ascii_digit() => {
+                // Continuing number
+                let mut new_num = num.clone();
+                new_num.bounding_box.end.x += 1;
+                new_num.value *= 10;
+                new_num.value += c.to_digit(10).unwrap() as usize;
+                self.current_number = Some(new_num);
+                (None, None)
+            },
+            (_, num) => {
+                let symb = Symbol {
+                    ch,
+                    coord: Coordinate {
+                        x: self.coord.x,
+                        y: self.coord.y,
+                    }
+                };
+                if num.is_some() {
+                    self.current_number = None;
+                    (Some(num.unwrap()), Some(symb))
+                } else {
+                    (None, Some(symb))
+                }
+            },
+
+        }
+    }
+}
+
+fn collision(bb: &BoundingBox, coord: &Coordinate) -> bool {
+    coord.x >= bb.start.x &&
+        coord.x <= bb.end.x &&
+        coord.y >= bb.start.y &&
+        coord.y <= bb.end.y
+}
 
 fn part_1(lines: Vec<String>) {
     let start = Instant::now();
@@ -48,7 +116,7 @@ fn part_1(lines: Vec<String>) {
     // scaling: numbers * symbols
 
     let mut numbers: Vec<Number> = Vec::new();
-    let mut symbols: Vec<Coordinate> = Vec::new();
+    let mut symbols: Vec<Symbol> = Vec::new();
     let mut chompy = StateMachine{
         coord: Coordinate{
             x: 0,
@@ -59,55 +127,14 @@ fn part_1(lines: Vec<String>) {
     for (y, line) in lines.into_iter().enumerate() {
         chompy.coord.x = 0;
         chompy.coord.y = y as isize;
-        // True == there is a number
-        // False == no number
         for char in line.chars() {
 
-            let num = chompy.current_number.clone();
-            match (char, num) {
-                ('.', Some(num)) => {
-                    // push into numbers, clear number, advance
-                    numbers.push(num);
-                    chompy.current_number = None;
-                },
-                ('.', None) => {},
-                (c, None) if c.is_ascii_digit() => {
-                    // number.start = x-1, y-1
-                    // number.end = x+1, y+1
-                    // number.value = char.parse()
-                    let number = Number{
-                        value: c.to_digit(10).unwrap() as usize,
-                        bounding_box: BoundingBox{
-                            start: Coordinate{
-                                x: chompy.coord.x-1,
-                                y: chompy.coord.y-1,
-                            },
-                            end: Coordinate {
-                                x: chompy.coord.x+1,
-                                y: chompy.coord.y+1,
-                            }
-                        }
-                    };
-                    chompy.current_number = Some(number);
-                },
-                (c, Some(num)) if c.is_ascii_digit() => {
-                    let mut new_num = num.clone();
-                    new_num.bounding_box.end.x += 1;
-                    new_num.value *= 10;
-                    new_num.value += c.to_digit(10).unwrap() as usize;
-                    chompy.current_number = Some(new_num);
-                },
-                (_, num) => {
-                    if num.is_some() {
-                        numbers.push(num.unwrap());
-                        chompy.current_number = None;
-                    }
-                    let coord = Coordinate{
-                        x: chompy.coord.x,
-                        y: chompy.coord.y,
-                    };
-                    symbols.push(coord);
-                },
+            let (num, symb) = chompy.consume_number(char);
+            if let Some(num) = num {
+                numbers.push(num);
+            }
+            if let Some(symb) = symb {
+                symbols.push(symb);
             }
             chompy.coord.x += 1;
         }
@@ -120,13 +147,10 @@ fn part_1(lines: Vec<String>) {
     let mut total = 0;
     for num in numbers {
         for symbol in symbols.clone() {
-            if symbol.x >= num.bounding_box.start.x
-                && symbol.x <= num.bounding_box.end.x
-                && symbol.y >= num.bounding_box.start.y
-                && symbol.y <= num.bounding_box.end.y {
-                    total += num.value;
-                    break;
-                }
+            if collision(&num.bounding_box, &symbol.coord) {
+                total += num.value;
+                break;
+            }
         }
     }
     println!("{}", total);
@@ -144,6 +168,61 @@ fn part_2(lines: Vec<String>) {
     // Find gears (* symbols which are adjacent to EXACTLY TWO numbers)
     // gear ratio is the product of the two numbers
     // sum all gear ratios
+    
+    // chomp
+    // go through symbols
+    // if the symbol isn't a *, skip
+    // if it is, check each number
+
+    let mut symbols: Vec<Symbol> = Vec::new();
+    let mut numbers: Vec<Number> = Vec::new();
+    let mut chompy = StateMachine{
+        coord: Coordinate{
+            x: 0,
+            y: 0,
+        },
+        current_number: None,
+    };
+    for (y, line) in lines.into_iter().enumerate() {
+        chompy.coord.x = 0;
+        chompy.coord.y = y as isize;
+        for char in line.chars() {
+            let (num, symb) = chompy.consume_number(char);
+            if let Some(num) = num {
+                numbers.push(num);
+            }
+            if let Some(symb) = symb {
+                symbols.push(symb);
+            }
+            chompy.coord.x += 1;
+        }
+        if chompy.current_number.is_some() {
+            numbers.push(chompy.current_number.clone().unwrap());
+            chompy.current_number = None;
+        }
+    }
+
+    let mut total = 0;
+
+    'symb: for symb in symbols {
+        if symb.ch != '*' {
+            continue;
+        }
+        let mut adjacent: Vec<Number> = Vec::new();
+
+        for num in numbers.clone() {
+            if collision(&num.bounding_box, &symb.coord) {
+                adjacent.push(num);
+            }
+            if adjacent.len() >= 3 {
+                continue 'symb;
+            }
+        }
+        if adjacent.len() == 2 {
+            total += adjacent[0].value * adjacent[1].value;
+        }
+    }
+    println!("{}", total);
 
     let duration = start.elapsed();
     println!("Time elapsed: {:?}", duration);
